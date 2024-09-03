1a38c8f02496be5550dcf6a0180476ba3c7b8560
Yan_ice <yan2364728692@gmail.com>
2024-09-03 20:39:07 +0800

    gitignore

--- Demo/CORTEX_A72_QEMU_MMK/build/home/yanice/Desktop/CORTEX_A72_QEMU_FreeRTOS/Port/GCC/ARM_AARCH64_QEMU/portASM.d
+++
@@ -1,2 +0,0 @@
-build//home/yanice/Desktop/CORTEX_A72_QEMU_FreeRTOS/Port/GCC/ARM_AARCH64_QEMU/portASM.o: \
- /home/yanice/Desktop/CORTEX_A72_QEMU_FreeRTOS/Port/GCC/ARM_AARCH64_QEMU/portASM.S
--- Demo/CORTEX_A72_QEMU_MMK/build/libmmk/os/src/mmk.d
+++
@@ -1,6 +0,0 @@
-build/libmmk/os/src/mmk.o: libmmk/os/src/mmk.c libmmk/os/include/mmk.h \
- libmmk/os/include/mmkdef.h
-
-libmmk/os/include/mmk.h:
-
-libmmk/os/include/mmkdef.h:
